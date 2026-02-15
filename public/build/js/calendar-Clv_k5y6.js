fac68f3247fd21604d4749b59805a8fffe20b215
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-02-16 00:38:54 +0100

    build prod assets

--- public/build/js/calendar-Clv_k5y6.js
+++
@@ -1,4 +0,0 @@
-import { default as o } from "./calendar/ConsecutiveDays-CVJm3n4V.js";
-export {
-  o as ConsecutiveDays
-};
--- public/build/js/index-BJ_oZpd8.js
+++
@@ -1,7 +0,0 @@
-/* empty css                               */
-import o from "./main/init-DoWhkRP3.js";
-console.debug("Welcome from Heimrich & Hannot Resource Booking");
-const r = o();
-export {
-  r as bookingForms
-};
