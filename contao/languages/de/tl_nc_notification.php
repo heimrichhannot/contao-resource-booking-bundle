98ee00cb0e483aae660c8e624cf26dee659b7806
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2026-02-18 09:23:46 +0100

    update: adjust German notification type translations for consistency

--- contao/languages/de/tl_nc_notification.php
+++ contao/languages/de/tl_nc_notification.php
@@ -6,6 +6,6 @@ use HeimrichHannot\ResourceBookingBundle\NotificationType\ReviewResponseNotifica
 
 $lang = &$GLOBALS['TL_LANG']['tl_nc_notification'];
 
-$lang['type'][OptInRequestNotificationType::NAME] = ['Buchungsanfrage Opt-In', 'Wird versendet, wenn der Nutzer seine E-Mail Adresse mittels Opt-In bestätigen muss.'];
-$lang['type'][ReviewRequestNotificationType::NAME] = ['Buchungsanfrage Prüfung ausstehend', 'Wird versendet, wenn eine Buchungsanfrage freigegeben werden muss.'];
-$lang['type'][ReviewResponseNotificationType::NAME] = ['Buchungsanfrage Geprüft', 'Wird versendet, wenn eine Buchungsanfrage geprüft wurde.'];
+$lang['type'][OptInRequestNotificationType::NAME] = ['Buchungsanfrage: Opt-In', 'Wird versendet, wenn der Nutzer seine E-Mail Adresse mittels Opt-In bestätigen muss.'];
+$lang['type'][ReviewRequestNotificationType::NAME] = ['Buchungsanfrage: Prüfung ausstehend', 'Wird versendet, wenn eine Buchungsanfrage freigegeben werden muss.'];
+$lang['type'][ReviewResponseNotificationType::NAME] = ['Buchungsanfrage: Geprüft', 'Wird versendet, wenn eine Buchungsanfrage geprüft wurde.'];
